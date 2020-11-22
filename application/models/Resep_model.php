b1f54d16f73217402d7f8a1a465091ad67ea029d
Razka173 <razka173@gmail.com>
2020-11-22 23:52:52 +0700

    fix bugs

--- application/models/Resep_model.php
+++ application/models/Resep_model.php
@@ -84,46 +84,48 @@ class Resep_model extends CI_model
             $this->db->like('nama', $nama);
         }
         else if ($bahan1 != null && $bahan2 != null) {
-
             // Find input to 'bahan' table
             $bahan = array($bahan1, $bahan2);
             $this->db->select('bahan.id');
             $this->db->from('bahan');
             $this->db->where_in('nama', $bahan);
+            
             // Place ID target to an 'bahan' array
             $bahan_ids = $this->db->get()->result_array();
             $bahan_id = array();
             foreach ($bahan_ids as $id) {
                 array_push($bahan_id, (int) $id['id']);
             }
-
-            // Find the 'bahan' in 'bahan_resep' table
-            $this->db->select('bahan_resep.resep_id');
-            $this->db->from('bahan_resep');
-            $this->db->where_in('bahan_id', $bahan_id);
-
-            // Place result into an array
-            $resep_ids = $this->db->get()->result_array();
-            $result = array();
-            foreach ($resep_ids as $resep_id) {
-                $this->db->select('*');
+            if(count($bahan_id)==2){
+                // Find the 'bahan' in 'bahan_resep' table
+                $this->db->select('bahan_resep.resep_id');
                 $this->db->from('bahan_resep');
-                $this->db->where('resep_id', $resep_id['resep_id']);
-                $this->db->where_in('bahan_id', $bahan_id);
-                $query = $this->db->get();
-                array_push($result, $query->row()->resep_id);
+                $this->db->where('bahan_id', $bahan_id[0]);
+
+                // Place result into an array
+                $resep_ids = $this->db->get()->result_array();
+                $result = array();
+                foreach ($resep_ids as $resep_id) {
+                    $this->db->select('*');
+                    $this->db->from('bahan_resep');
+                    $this->db->where('resep_id', $resep_id['resep_id']);
+                    $this->db->where('bahan_id', $bahan_id[1]);
+                    $query = $this->db->get()->row();
+                    if($query == null){
+                    continue;
+                    }
+                    array_push($result, $query->resep_id);
+                }
+            if(count($result) > 0){
+                $this->db->select('*');
+                $this->db->from('resep');
+                $this->db->where_in('id', $result);
+                $this->db->limit($limit);
+                if ($order) $this->db->order_by($order, "DESC");
+                return $this->db->get()->result_array();
             }
-            
-            $this->db->select('*');
-            $this->db->from('resep');
-            $this->db->where_in('id', $result);
-            $this->db->limit($limit);
-            if ($order) $this->db->order_by($order, "DESC");
-            return $this->db->get()->result_array();
+            }
         }
-        $this->db->limit($limit);
-        if ($order) $this->db->order_by($order, "DESC");
-        return $this->db->get('resep')->result_array();
     }
 
     public function createResep($data)
