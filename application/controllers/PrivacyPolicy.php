4cf117f265b797b4541a97a955cf19d48846a8ae
Razka173 <razka173@gmail.com>
2020-12-02 21:13:05 +0700

    fix bugs

--- application/controllers/PrivacyPolicy.php
+++ application/controllers/PrivacyPolicy.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PrivacyPolicy extends CI_Controller {
+class Privacypolicy extends CI_Controller {
 
 	public function index()
 	{
