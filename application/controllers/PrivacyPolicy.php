287da7674b819b303d804b03524adafc42a26052
Razka173 <razka173@gmail.com>
2020-12-02 18:46:17 +0700

    Resep Users

--- application/controllers/PrivacyPolicy.php
+++ application/controllers/PrivacyPolicy.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Privacy_Policy extends CI_Controller {
+class PrivacyPolicy extends CI_Controller {
 
 	public function index()
 	{
 		$data = array(	'title' 	=> 'Privacy Policy',
-						'isi'		=> 'about/privacy_policy/list'
+						'isi'		=> 'about/privacypolicy/list'
 					);
 		$this->load->view('about/layout/wrapper', $data, FALSE);
 	}
